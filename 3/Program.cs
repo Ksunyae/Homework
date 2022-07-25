08220e25a19f633fe6ca308da0d8113b0ff326e7
Kseniya <keusy@yandex.ru>
2022-07-25 13:58:55 +0300

    add 3

---
+++ 3/Program.cs
@@ -0,0 +1,10 @@
+﻿int n = Convert.ToInt32(Console.ReadLine());
+if (n == 1)
+    Console.WriteLine("x > 0, y > 0");
+if (n == 2)
+    Console.WriteLine("x > 0, y < 0");
+if (n == 3)
+    Console.WriteLine("x < 0, y < 0");
+if (n == 4)
+    Console.WriteLine("x < 0, y > 0");
+
