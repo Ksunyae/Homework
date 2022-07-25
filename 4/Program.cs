3a88993c87192c307d3e5bdfb0f4ff0b760a3303
Kseniya <keusy@yandex.ru>
2022-07-25 14:08:20 +0300

    add 4

---
+++ 4/Program.cs
@@ -0,0 +1,8 @@
+﻿int x1 = Convert.ToInt32(Console.ReadLine());
+int y1 = Convert.ToInt32(Console.ReadLine());
+int x2 = Convert.ToInt32(Console.ReadLine());
+int y2 = Convert.ToInt32(Console.ReadLine());
+
+double distance = Math.Sqrt(((x2-x1)*(x2 - x1)) + ((y2 - y1) * (y2 - y1)));
+
+Console.WriteLine("Расстояние межд точками равно " + distance);
