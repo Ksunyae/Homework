1fa339a26bca04591b8d61e371508b913906ac78
Kseniya <keusy@yandex.ru>
2022-07-27 13:30:22 +0300

    add 9

---
+++ 9/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите пятизначное число:");
+string nstr = Console.ReadLine();
+if ((nstr[0] != nstr[4]) && (nstr[1] != nstr[3]))
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+    Console.Write("No");
+}
